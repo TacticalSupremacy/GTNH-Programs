2aeb51088fd2fbb7d001b597b9eb0654afa8d1a0
Ariel Rin <ozirascal@gmail.com>
2024-02-14 00:20:21 +1000

    lua indexing again

--- bloodaltar/blood-altar.lua
+++ bloodaltar/blood-altar.lua
@@ -151,27 +151,27 @@ term.clear()
 while true do
     LifeEssenceStatus()
     if GetItem("Arcane Slate") ~= nil and GetItem("Blank Slate") ~= nil then
-        if GetItem("Arcane Slate") > 0 and GetItem("Blank Slate") < slateConfig[1]["target"] then
+        if GetItem("Arcane Slate") > 0 and GetItem("Blank Slate") < slateConfig[2]["target"] then
             CraftSlate(1, 2)
         end
     end
     if GetItem("Blank Slate") ~= nil and GetItem("Reinforced Slate") ~= nil then
-        if bloodAltarTier >= 2 and GetItem("Blank Slate") > 0 and GetItem("Reinforced Slate") < slateConfig[2]["target"] then
+        if bloodAltarTier >= 2 and GetItem("Blank Slate") > 0 and GetItem("Reinforced Slate") < slateConfig[3]["target"] then
             CraftSlate(2, 3)
         end
     end
     if GetItem("Reinforced Slate") ~= nil and GetItem("Imbued Slate") ~= nil then
-        if bloodAltarTier >= 3 and GetItem("Reinforced Slate") > 0 and GetItem("Imbued Slate") < slateConfig[3]["target"] then
+        if bloodAltarTier >= 3 and GetItem("Reinforced Slate") > 0 and GetItem("Imbued Slate") < slateConfig[4]["target"] then
             CraftSlate(3, 4)
         end
     end
     if GetItem("Imbued Slate") ~= nil and GetItem("Demonic Slate") then
-        if  bloodAltarTier >= 4 and GetItem("Imbued Slate") > 0 and GetItem("Demonic Slate") < slateConfig[4]["target"] then
+        if  bloodAltarTier >= 4 and GetItem("Imbued Slate") > 0 and GetItem("Demonic Slate") < slateConfig[5]["target"] then
             CraftSlate(4, 5)
         end
     end
     if GetItem("Demonic Slate") ~= nil and GetItem("Ethereal Slate") ~= nil then
-        if bloodAltarTier >= 5 and GetItem("Demonic Slate") > 0 and GetItem("Ethereal Slate") < slateConfig[5]["target"] then
+        if bloodAltarTier >= 5 and GetItem("Demonic Slate") > 0 and GetItem("Ethereal Slate") < slateConfig[6]["target"] then
         CraftSlate(5, 6)
         end
     end
